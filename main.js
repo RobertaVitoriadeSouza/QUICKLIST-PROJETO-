e36a3ffda5255a4d05cfeec5c02b62b9f9cea65c
Roberta Vitoria <roberta.vaibrilhar@gmail.com>
2025-06-09 10:37:02 -0300

    crisçso da funçao showItemslist para renderizar na tela os itens do arry

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ function addItem(item) {
 
     document.querySelector('#item').value = ''
 }
+function showItemsList() {
+    const sectionlist = document.querySelector(".list")
+    itens.map((item, index) => {
+       sectionlist.innerHTML += `
+        <div class="item">
+        <div>
+        <input type="checkbox" name="list" id= 'item-${index}' >
+
+        <div class = "custom-checkbox">
+         <img src="./assets/assets/checked.svg" alt="checked">
+        </div>
+        <label for="item-${index}">${item.name}</label>
+        </div>
+
+        <button>
+        <img src="./assets/assets/trash-icon.svg" alt="trash icon">
+        </button>
+        </div>
+    })
+
+    }
